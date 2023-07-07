cda02cf8247ff22a2ba6051b05e0a73d05942fb5
Juan Senen <juansenen@hotmail.com>
2023-07-07 09:25:33 +0200

    Add Conecction

--- controller/start_controller.php
+++ controller/start_controller.php
@@ -1,4 +1,8 @@
 <?php
+include './domain/Conection.php';
+
+$conecction = new Conecction();
+$dbh = $conecction->getConection();
 
 function iniweb(){
     include 'view/headerview.php';
---
+++ domain/Conection.php
@@ -0,0 +1,30 @@
+<?php
+class Conecction
+{
+
+    function __construct()
+    {
+    }
+    //Funcion para conectar
+    function getConection()
+    {
+        // Con un array de opciones
+        try {
+            $dbname = "emilidatabase"; //Nombre de la Base de Datos
+            $user = "root"; // Usuario
+            $password = "root"; // Contraseña
+            $server = 'localhost'; // Dirección servidor
+            $dbh = "";
+            $dsn = "mysql:host=$server;dbname=$dbname;charset=UTF8";
+            $dbh = new PDO($dsn, $user, $password);
+            $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+        } catch (PDOException $e) {
+            echo $e->getMessage();
+            redirect('errorpagePDO_view.php');
+        }
+        return $dbh;
+    }
+
+}
+?>
