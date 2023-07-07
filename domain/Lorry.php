1b0fa93a24cc05f39d710a6698838dafee6bba79
Juan Senen <juansenen@hotmail.com>
2023-07-07 13:02:23 +0200

    Class conecction and lorry

---
+++ domain/Lorry.php
@@ -0,0 +1,12 @@
+<?php
+class Lorry
+{
+    private long $id;
+    private String $brand;
+    private int $km;
+    private String $model;
+
+public function __construct(){}
+
+}
+?>
--- view/headerview.php
+++ view/headerview.php
@@ -28,7 +28,7 @@ $(function() {
     <!-- Start vertical navbar -->
     <header id="header">
        <div class="vertical-nav bg-white" id="sidebar">
-          <div class="py-4 px-3 mb-2 mt-2 bg-light">
+          <div class="py-4 px-3 mb-2 mt-2 bg-dark">
             <div class="align-items-center" id="half">    
             <div id="image" >
              <img src="resources/img/NOSTROMO-LOGISTIC_logo.png" alt="...">
@@ -63,25 +63,13 @@ $(function() {
     <!-- Toggle button -->
         <button id="sidebarCollapse" type="button" class="btn btn-light bg-white rounded-pill px-4 mb-4"><i class="fa fa-bars mr-2"></i><small class="text-uppercase font-weight-bolder">Esconder</small></button>
       
-    <!-- Content Header -->   
-        <h2 class="display-4 font-weight-bold text-center text-white">NOSTROMO LOGISTIC</h2>
-        <div class="separator mt-4"></div>
-      
-    <!-- content -->
-        <figure class="mb-4" id="img-div">
-           <div id="image" >
-             <img src="resources/img/NOSTROMO-LOGISTIC_logo.png" alt="...">
-           </div>
-         </figure>
-  
+    <!-- content -->  
         <div class="row text-white">
-    <div class="col-lg-7 mx-auto">
-      <p class="lead">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-      <p class="lead text-white">Here's a list of<a href="#" class="text-white">
-        <u>upcoming shows</u></a>.
-  </p>
-    </div>
-  </div>
+           <div class="col-lg-7 mx-auto">
+              <p class="lead">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+              <p class="lead text-white">Here's a list of<a href="#" class="text-white"></p>
+            </div>
+        </div>
       
     </div>
       
